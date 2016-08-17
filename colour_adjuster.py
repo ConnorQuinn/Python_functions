73f708dd9c60710351b483f6eb75dce3e72de6cb
Connor Quinn <quinn.connor@gmail.com>
2016-08-17 21:59:22 +0100

    Started to abstract script into a function

--- colour_adjuster.py
+++ colour_adjuster.py
@@ -38,36 +38,40 @@ DIR = os.path.dirname(os.path.abspath(__file__))
 IMAGE_DIR = os.path.join(DIR, 'Main_data')
 OUT_DIR = os.path.join(DIR, 'Main_data')
 
-image_in_path = os.path.join(IMAGE_DIR, 'Uncorrected_main_bars_jan16.png') # get higher res .png
+image_in = os.path.join(IMAGE_DIR, 'Uncorrected_main_bars_jan16.png')
 image_list = os.listdir(IMAGE_DIR)
 image_out_path = (os.path.join(OUT_DIR, 'Adjusted_uncorrected_main_bars_jan16.png'))
 
 
-im = Image.open(image_in_path)
-im = im.convert('RGBA')
-data = np.array(im)   # "data" is a height x width x 4 numpy array
-red, green, blue, alpha = data.T
-
-#orig_cols = [(65,127,255), (64,119,187),(255,127,127),(242,125,128)] # localiser
-#tar_cols  = [(65,120,250),(65,120,250),(255,100,100),(255,100,100)] # localiser
-#orig_cols = [(0,127,255), (255,127,127)]
-#tar_cols  = [(65,120,250),(255,100,100)]
-#           [light blue,  light blue,  dark blue, dark blue,   light red,...
-#            light red,     dark red]
-#orig_cols = [(0, 127, 255), (64, 119, 187), (56, 83, 164), (0, 0, 255),
-#             (242, 125, 128), (255, 127, 127), (237, 42, 44)]
 orig_cols = [(65, 120, 250), (55, 50, 200), (225, 2, 2), (255, 100, 100) ]
-
 tar_cols = [(122, 194, 254), (0, 0, 160), (233, 28, 22),(255, 187, 187)]
 
-for orig_col, tar_col in zip(orig_cols, tar_cols):
-    # Replace blues with one blue... (leaves alpha values alone...)
-    colours_to_change = (red == orig_col[0])&(green == orig_col[1])&(blue == orig_col[2])
-    data[..., :-1][colours_to_change.T] = tar_col # Transpose back needed
+def change_colours(image_in, orig_col, tar_col):
+    """Replace specific colours in a raster image.
+    
+       args: 
+       (1) The image that you want to change. 
+       (2) A list of colours that you want to remove.
+       (3) A list of the colours you want to replace them.
 
-im2 = Image.fromarray(data)
-im2.show()
-im2.save(image_out_path)
+       returns: an image file with the adjusted colours
+    """
+    im = Image.open(image_in)
+    im = im.convert('RGBA')
+    data = np.array(im)   # "data" is a height x width x 4 numpy array
+    red, green, blue, alpha = data.T
+
+    for orig_col, tar_col in zip(orig_cols, tar_cols):
+        # Replace blues with one blue... (leaves alpha values alone...)
+        colours_to_change = (red == orig_col[0])&(green == orig_col[1])&(blue == orig_col[2])
+        data[..., :-1][colours_to_change.T] = tar_col # Transpose back needed
+
+    im_out = Image.fromarray(data)
+    im_out.show()
+    return im_out
+
+
+im_out.save(image_out_path)
 
 
     
